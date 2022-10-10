c513158d74e5ded7a7838efa2c24458a3cd81afb
aibek23 <aibekasanakunuulu@gmail.com>
2022-10-10 11:09:08 +0600

    build 1

--- app.js
+++ app.js
@@ -65,6 +65,12 @@ const saveData = async (data, username) => {
 
 app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
+if(process.env.NODE_ENV === 'production'){
+	app.use('/',express.static(path.join(__dirname,'client','build')))
+	app.get('*',(req,res)=>{
+		res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+	})
+}
 // app.use('/api/toCall', require('./jingle/create.channel'))
 const io = require("socket.io")(server, {
 	cors: {
