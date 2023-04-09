4c83ce9b54dbd36c6e4a798ec9474228359232a0
kalygul <aibekasanakunuulu@gmail.com>
2023-04-09 13:47:19 +0300

    PrivacyPolicy_index.html

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,17 +5,20 @@ import styles from "../css/HomePage.module.css"
 export const HomePage = () => {
 
     return(
+    <>
     <div className="Home_Page">
         <div className="blur">  
         <div className="container">
             <div className={styles.title}>
             <h2>surdo.kg</h2>
-            <h4>сервис дистанционного сурдоперевода, который позволяет организациям обеспечивать доступную среду для инвалидов по слуху с минимальными затратами, а глухим получить доступ к коммуникациям наравне со всеми.</h4>
-            <Link className="nav-link" to="/" >выйти</Link>
+            <h4>сервис дистанционного сурдоперевода, который позволяет организациям обеспечивать доступную среду для инвалидов по слуху с минимальными затратами, 
+                а глухим получить доступ к коммуникациям наравне со всеми.</h4>
             </div>
         </div>
         </div>
     </div>
+    <div className="text-center"><Link className="nav-link" to="/PrivacyPolicy" >Политика конфиденциальности</Link></div>
+    </>
     )
 
 }
